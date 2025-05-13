ac5c48bf1efd4ffa47225fd2ba5699f6b257ed1b
richh-brreto <richard.barreto@sptech.school>
2025-05-13 13:27:33 -0300

    cadastro e login funcionais

--- API/src/controllers/usuarioController.js
+++ API/src/controllers/usuarioController.js
@@ -16,7 +16,10 @@ function autenticar(req, res) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
 
-                    if (resultadoAutenticar.length == 0) {
+                    if (resultadoAutenticar.length == 1) {
+                        res.status(200).json(resultadoAutenticar);
+                    
+                    } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
